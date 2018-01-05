a17cdd6437498ea855458bc1487f1f6f547ef74a
marek.talpas <marek.talpas@globallogic.com>
2018-01-05 15:58:14 +0100

    created POST /users route handler

--- server/server.js
+++ server/server.js
@@ -108,6 +108,24 @@ app.patch('/todos/:id', (req, res) => {
 
 });
 
+app.post('/users', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  var user = new User(body);
+
+  user
+    .save()
+    .then(() => {
+      return user.generateAuthToken();
+    })
+    .then((token) => {
+      res.header('x-auth', token).send(user);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
 app.listen(PORT, () => {
   console.log(`app is listening on the port ${PORT}`);
 });
